a324fa43c3222add7e89fb7dff9d8b2408a15fb4
Justin Watt <justinwatt@gmail.com>
2016-08-21 21:13:01 -0700

    Use public facing types for serializing db values

--- src/Api/User.hs
+++ src/Api/User.hs
@@ -13,6 +13,7 @@ import           Database.Persist.Postgresql (Entity (..), fromSqlKey, insert,
 import           Network.Wai                 (Application)
 import           Servant
 import           Servant.JS                  (vanillaJS, writeJSForAPI)
+import qualified Database.Esqueleto as E
 
 import           Config                      (App (..), Config (..))
 import           Models
@@ -20,11 +21,18 @@ import           Models
 type UserAPI =
          "users" :> Get '[JSON] [Entity User]
     :<|> "users" :> Capture "name" String :> Get '[JSON] (Entity User)
-    :<|> "users" :> ReqBody '[JSON] User :> Post '[JSON] Int64
+    :<|> "users" :> ReqBody '[JSON] User :> Servant.Post '[JSON] Int64
+    :<|> "user" :> Capture "id" UserId :> "posts" :> Get '[JSON] [Models.PublicPost]
+    :<|> "user" :> Capture "id" UserId :> "posts" :> ReqBody '[JSON] Models.Post :> Servant.Post '[JSON] Int64
 
 -- | The server that runs the UserAPI
 userServer :: ServerT UserAPI App
-userServer = allUsers :<|> singleUser :<|> createUser
+userServer =
+       allUsers
+  :<|> singleUser
+  :<|> createUser
+  :<|> allUserPosts
+  :<|> createUserPost
 
 -- | Returns all users in the database.
 allUsers :: App [Entity User]
@@ -46,6 +54,25 @@ createUser :: User -> App Int64
 createUser p = do
     newUser <- runDb (insert (User (userName p) (userEmail p)))
     return $ fromSqlKey newUser
+
+allUserPosts :: UserId -> App [PublicPost]
+allUserPosts uID = do
+  posts <- runDb $ E.select $
+                   E.from $ \p -> do
+                   E.where_ $ p E.^. PostAuthorId E.==. E.val uID
+                   return p
+
+  return $ map (\(Entity i p) -> postToPublicPost i p) posts
+
+createUserPost :: UserId -> Models.Post -> App Int64
+createUserPost uID p =
+  if uID /= postAuthorId p
+    then
+      throwError err409
+    else do
+      newPost <- runDb $ insert $ Models.Post (postTitle p) (postBody p) (postAuthorId p)
+      return $ fromSqlKey newPost
+
 
 -- | Generates JavaScript to query the User API.
 generateJavaScript :: IO ()
--- src/Models.hs
+++ src/Models.hs
@@ -14,7 +14,8 @@
 module Models where
 
 import           Control.Monad.Reader
-import           Data.Aeson           (FromJSON, ToJSON)
+import           Data.Aeson           (FromJSON, ToJSON, (.=), toJSON, object,
+                                       (.:), parseJSON, Value(Object))
 import           Database.Persist.Sql
 import           Database.Persist.TH  (mkMigrate, mkPersist, persistLowerCase,
                                        share, sqlSettings)
@@ -27,7 +28,28 @@ User json
     name String
     email String
     deriving Show
+
+Post json
+    title String
+    body String
+    authorId UserId
+    deriving Show
 |]
+
+data PublicPost =
+  PublicPost
+  { id :: PostId
+  , title :: String
+  , body  :: String
+  } deriving (Eq, Show, Generic)
+
+postToPublicPost :: PostId -> Post -> PublicPost
+postToPublicPost pid Post{..} =
+  PublicPost { title = postTitle, body = postBody, id = pid }
+
+instance ToJSON PublicPost where
+
+instance FromJSON PublicPost where
 
 doMigrations :: SqlPersistT IO ()
 doMigrations = runMigration migrateAll
